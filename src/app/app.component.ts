72df7a229d4204d3a32bcb37fdca999b00455e34
reenunehra <53217952+reenunehra@users.noreply.github.com>
2022-05-05 19:05:23 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'blog';
+  data = 'by ReenuNehra';
+
+  //  getData(val: string){
+
+  //   console.warn(val);
+
+  //  }
+  // ----get text box value and print-------
+  // displayval = ' ';
+  // getUsername(val: string) {
+  //   console.log(val);
+  //   this.displayval = val;
+  // }
+
+// count = 0;
+// counter(type:string){
+// type=== 'add' ? this.count++ : this.count--;
+// }
+
+// name = "reenu" 
+// disable = true
+
+
+show = false
 }
