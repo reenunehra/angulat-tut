413c0f6622c713b8e41696944c4f23936afc7688
reenunehra <53217952+reenunehra@users.noreply.github.com>
2022-05-06 12:52:26 +0530

    condition statement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,5 +30,47 @@ export class AppComponent {
 // disable = true
 
 
-show = false
+// show = false
+
+// color =  "frefrf";
+
+// users = ['Raam','Shaam','Peter','Sam','Rokky','Heyat'];
+
+// userDetails = [
+//   {name : 'Raam' , email : 'raam123@gmail.com'},
+//   {name : 'Shaam' , email : 'shaam13@gmail.com'},
+//   {name : 'peter' , email : 'peter@gmail.com'},
+//   {name : 'sam' , email : 'sam343@gmail.com'},
+//   {name : 'Rokky' , email : 'rokk23@gmail.com'},
+//   {name : 'heyat' , email : 'heyat2356@gmail.com'},
+// ]
+
+// users = [
+//   {name : 'Raam' , email : 'raam123@gmail.com', socialAccount:['facebook','insta','whtp']},
+//   {name : 'Shaam' , email : 'shaam13@gmail.com', socialAccount:['facebook','insta','whtp']},
+//   {name : 'peter' , email : 'peter@gmail.com', socialAccount:['facebook','insta','whtp']},
+// ]
+ 
+
+colors = "color-update"
+color="red";
+
+change(){
+  this.color="blue"
 }
+
+
+
+}
+// -----clouser------------------
+
+// function addition (a) {
+//   let b = 20;
+//   function sumval(){
+//     let sum =a + b;
+//     console.log('${sum}')
+//   }
+//   sumval();
+// }
+// addition(5);
+
